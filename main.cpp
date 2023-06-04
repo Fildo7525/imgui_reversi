68542260f9b89e953b52d62490652f813372b7df
Filip Lobpreis <f.lobpreis@gmail.com>
2023-06-04 13:53:51 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "HalloWorld.h"
+
+int main(int argc, char *argv[])
+{
+	std::cout << "Hello World!\n";
+	return 0;
+}
+
---
+++ src/HalloWorld.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+
+class HalloWorld
+{
+public:
+	HalloWorld() = default;
+
+};
+
